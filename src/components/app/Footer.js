21c277999f3c294872cfd77c06d5fae8244679e8
Ian Parcon <ianparcs@gmail.com>
2022-03-24 19:57:01 +0800

    update version

--- src/components/app/Footer.js
+++ src/components/app/Footer.js
@@ -40,7 +40,7 @@ export default function Footer(props) {
             <Row className="pb-2">
                 <Col>
                     <p className="text-white p-0 m-0">
-                        Built with ReactJS | v1.3.2
+                        Built with ReactJS | v1.3.3
                         <br/>
                     </p>
                     <div>
